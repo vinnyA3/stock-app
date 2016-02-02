50ee452b88a743b983aaab827c4bc19d555938e6
vinnya3 <vinaceto@yahoo.com>
2016-02-02 18:31:41 -0500

    added stock function

--- public/app/controllers/portfolioCtrl.js
+++ public/app/controllers/portfolioCtrl.js
@@ -1,4 +1,22 @@
-angular.module('portfolioCtrl',[])
-	.controller('portfolioController', function(){
+angular.module('portfolioCtrl',['stockService'])
+	.controller('portfolioController', function(Stock){
+		var vm = this;
+	
+		//get our stocks and them to the view
+		vm.getStocks = function(){
+			Stock.getUserStocks()
+				.then(function(data){
+					console.log(data);
+					//set the vm's userData property (contains stocks)
+					vm.userData = data;
+				})
+				.catch(function(data){
+					vm.error = true;
+					vm.errorMessage = 'There was an error retrieving your stocks.';
+				})
+		};
+	
+		//call our get stock function
+		vm.getStocks();
 		
 	});
