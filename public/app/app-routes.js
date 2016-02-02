a3ac4c235c3fb6b75a094b1beb4c6a576e23e030
vinnya3 <vinaceto@yahoo.com>
2016-02-02 13:09:49 -0500

    added portfolio config

--- public/app/app-routes.js
+++ public/app/app-routes.js
@@ -9,16 +9,37 @@ angular.module('appRoutes',['ui.router'])
 			.state('signup',{
 				url:'/signup',
 				templateUrl: 'app/views/pages/signup.html',
-				controller: 'controller as signup'
+				controller: 'signupController as signup'
 			})
 			.state('login',{
 				url:'/login',
 				templateUrl: 'app/views/pages/login.html',
-				controller: 'controller as login'
+				controller: 'loginController as login'
+			})
+			.state('portfolio',{
+				url:'/portfolio',
+				templateUrl: 'app/views/pages/portfolio.html',
+				controller: 'portfolioController as portfolio',
+				resolve: {
+					loginRequired: loginRequired
+				}
 			});
 	
 		$urlRouterProvider.otherwise('/');
 		
 		$locationProvider.html5Mode(true);
+	
+	
+		//========================  LOGIN REQUIRED RESOLVE FUNCTION  ===========================
+		function loginRequired($q,$location,$auth){
+			var deffered = $q.defer();
+			if($auth.isAuthenticated()){
+			   	deffered.resolve();
+			}else{
+				//redirect to home page
+				$location.path('/');
+			}
+			return deffered.promise;
+		}
 	
 	});
