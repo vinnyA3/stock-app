0af2d4f234b3cc1db4911ba373b10129d4a34eeb
vinnya3 <vinaceto@yahoo.com>
2016-02-02 18:30:38 -0500

    added new route config

--- public/app/app-routes.js
+++ public/app/app-routes.js
@@ -3,23 +3,42 @@ angular.module('appRoutes',['ui.router'])
 		$stateProvider
 			.state('home',{
 				url:'/',
-				templateUrl:'app/views/pages/home.html',
+				views: {
+					'': {templateUrl: 'app/views/pages/home.html'},
+					'nav@home': {templateUrl: 'app/views/pages/partials/navbar-home-transparent.html',controller:'navbarController as nav'}
+				},
 				controller: 'mainController as main'
 			})
 			.state('signup',{
 				url:'/signup',
-				templateUrl: 'app/views/pages/signup.html',
-				controller: 'signupController as signup'
+				views:{
+					'':{templateUrl:'app/views/pages/signup.html', controller: 'signupController as signup'},
+					'nav@signup':{templateUrl:'app/views/pages/partials/navbar.html', controller:'navbarController as nav'}
+				}
 			})
 			.state('login',{
 				url:'/login',
-				templateUrl: 'app/views/pages/login.html',
-				controller: 'loginController as login'
+				views:{
+					'':{templateUrl:'app/views/pages/login.html', controller: 'loginController as login'},
+					'nav@login':{templateUrl:'app/views/pages/partials/navbar.html', controller:'navbarController as nav'}
+				}
 			})
 			.state('portfolio',{
 				url:'/portfolio',
-				templateUrl: 'app/views/pages/portfolio.html',
-				controller: 'portfolioController as portfolio',
+				views:{
+					'':{templateUrl:'app/views/pages/portfolio.html', controller: 'portfolioController as portfolio'},
+					'nav@portfolio':{templateUrl:'app/views/pages/partials/navbar.html', controller:'navbarController as nav'}
+				},
+				resolve: {
+					loginRequired: loginRequired
+				}
+			})
+			.state('hotstocks',{
+				url: '/hotstocks',
+				views:{
+					'':{templateUrl:'app/views/pages/hotStocks.html'},
+					'nav@hotstocks':{templateUrl:'app/views/pages/partials/navbar.html', controller:'navbarController as nav'}
+				},
 				resolve: {
 					loginRequired: loginRequired
 				}
