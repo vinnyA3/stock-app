829b39533003e30294ae6e4de7af9ae5aa80d580
vinnya3 <vinaceto@yahoo.com>
2016-02-02 02:40:02 -0500

    added model reference

--- app/models/Stock.js
+++ app/models/Stock.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var StocksSchema = mongoose.Schema({
 	name: String,
 	symbol: String,
-	created_by: {type: mongoose.SchemaTypes.ObjectId}
+	created_by: {type: mongoose.SchemaTypes.ObjectId, ref:'User'}
 });
 
 //export the model
