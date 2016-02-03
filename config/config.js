8c8c5bf49a55665b6f006bf2f20039edd3dfb591
vinnya3 <vinaceto@yahoo.com>
2016-02-03 01:27:27 -0500

    minor changes

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	//'db' : 'mongodb://127.0.0.1:27017/stockapp_db', //local
-	'db' : 'mongodb://ace:herby@ds055565.mongolab.com:55565/stock_app', //remote
+	'db' : 'mongodb://127.0.0.1:27017/stockapp_db', //local
+	//'db' : 'mongodb://ace:herby@ds055565.mongolab.com:55565/stock_app', //remote
 	'secret' : 'thebayharborbutcherwasborninblood'
 };
