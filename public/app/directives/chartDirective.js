0e94b063899f879682cfa89de0c3a650c9dadd3f
vinnya3 <vinaceto@yahoo.com>
2016-02-24 15:09:39 -0500

    added viewport rendering, meta data

--- public/app/directives/chartDirective.js
+++ public/app/directives/chartDirective.js
@@ -5,9 +5,59 @@ angular.module('chartDirective',[])
 			templateUrl:'app/views/pages/partials/chart-partial.html',
 			replace:true,
 			link: function(scope,element,attrs){
-				element.find('button').click(function(){
-					console.log('button clicked');
-				});	
+				
+			var seriesOptions = [],
+			seriesCounter = 0,
+			names = ['IBM','MSFT', 'AAPL', 'GOOG'];	
+	
+			function createChart(){
+
+				//add service here
+				   $('.stock-graph').highcharts('StockChart', {
+						rangeSelector: {
+							selected: 4
+						},
+						yAxis: {
+							labels: {
+								formatter: function () {
+									return (this.value > 0 ? ' + ' : '') + this.value + '%';
+								}
+							},
+							plotLines: [{
+								value: 0,
+								width: 2,
+								color: 'silver'
+							}]
+						},
+						plotOptions: {
+							series: {
+								compare: 'percent'
+							}
+						},
+						tooltip: {
+							pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+							valueDecimals: 2
+						},
+						series: seriesOptions
+					});
+
+				}; //end chart function
+	
+			    $.each(names, function (i, name) {
+					//refactor into service
+        $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=' + name.toLowerCase() + '-c.json&callback=?',    function (data) {
+            seriesOptions[i] = {
+                name: name,
+                data: data
+            };
+            // As we're loading the data asynchronously, we don't know what order it will arrive. So
+            // we keep a counter and create the chart when all the data is loaded.
+            seriesCounter += 1;
+            if (seriesCounter === names.length) {
+                createChart();
+            }
+        });
+    });
 			},
 			controller: function($scope){
 				console.log('working....');
